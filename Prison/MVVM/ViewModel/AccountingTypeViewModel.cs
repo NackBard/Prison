b58e37e365d4e30f603703294b8c66d0f7c987fb
NackBard <artemguskov5@gmail.com>
2021-11-30 09:26:32 +0300

    Реализовал таблицу AccountingType

--- Prison/MVVM/ViewModel/AccountingTypeViewModel.cs
+++ Prison/MVVM/ViewModel/AccountingTypeViewModel.cs
@@ -1,12 +1,137 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using Prison.Core;
+using Prison.Data;
+using Prison.Model;
+using System.Collections.ObjectModel;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Prison.MVVM.ViewModel
 {
-    internal class AccountingTypeViewModel
+    class AccountingTypeViewModel : TableViewModel, ICRUD, ITableModel
     {
+        private ObservableCollection<AccountingType> _accountingTypes;
+        public ObservableCollection<AccountingType> AccountingTypes
+        {
+            get => _accountingTypes;
+            set
+            {
+                _accountingTypes = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private ObservableCollection<AccountingType> _accountingTypesDelete;
+        public ObservableCollection<AccountingType> AccountingTypesDelete
+        {
+            get => _accountingTypesDelete;
+            set
+            {
+                _accountingTypesDelete = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private AccountingType _accountingTypeDeleteSelected;
+        public AccountingType AccountingTypeDeleteSelected
+        {
+            get => _accountingTypeDeleteSelected;
+            set
+            {
+                _accountingTypeDeleteSelected = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private AccountingType _accountingTypeSelected;
+        public AccountingType AccountingTypeSelected
+        {
+            get => _accountingTypeSelected;
+            set
+            {
+                _accountingTypeSelected = value;
+                AccountingTypeForEdit = (AccountingType)_accountingTypeSelected?.Clone() ?? new AccountingType();
+                OnPropertyChanged();
+            }
+        }
+        private AccountingType _accountingTypeForEdit;
+        public AccountingType AccountingTypeForEdit
+        {
+            get => _accountingTypeForEdit;
+            set
+            {
+                _accountingTypeForEdit = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public bool CanDelete => AccountingTypeSelected != null;
+        public bool CanCreate => Validate();
+        public bool CanUpdate => AccountingTypeSelected != null;
+        public bool CanClear => AccountingTypesDelete.Count > 0;
+        public bool CanRecover => AccountingTypeDeleteSelected != null;
+
+        public AccountingTypeViewModel()
+        {
+            AccountingTypeForEdit = new AccountingType();
+            AccountingTypes = new ObservableCollection<AccountingType>();
+            AccountingTypesDelete = new ObservableCollection<AccountingType>();
+            Init();
+        }
+        public void Init()
+        {
+            ReadAsync();
+            DeleteCommand = new RelayCommand(o => Drop(), param => CanDelete);
+            CreateCommand = new RelayCommand(o => CreateAsync());
+            UpdateCommand = new RelayCommand(o => UpdateAsync(), param => CanUpdate);
+            RecoverCommand = new RelayCommand(o => Recover(), param => CanRecover);
+            ClearCommand = new RelayCommand(o => DeleteAsync(), param => CanClear);
+        }
+        public void Recover()
+        {
+            AccountingTypes.Add(AccountingTypeDeleteSelected);
+            AccountingTypesDelete.Remove(AccountingTypeDeleteSelected);
+        }
+        public void Drop()
+        {
+            AccountingTypesDelete.Add(AccountingTypeSelected);
+            AccountingTypes.Remove(AccountingTypeSelected);
+        }
+
+        public async void CreateAsync()
+        {
+            if (CanCreate)
+            {
+                AccountingTypeForEdit.Id = null;
+                await DataSender.PostRequest(nameof(AccountingTypes), AccountingTypeForEdit);
+                ReadAsync();
+            }
+        }
+
+        public async void DeleteAsync()
+        {
+            foreach (var item in AccountingTypesDelete)
+            {
+                var s = await DataSender.DeleteRequest(nameof(AccountingTypes), item.Id.Value);
+            }
+            AccountingTypesDelete.Clear();
+            ReadAsync();
+        }
+
+        public async void ReadAsync()
+        {
+            AccountingTypes = await ApiConnector.GetAll<AccountingType>(nameof(AccountingTypes));
+            foreach (var item in AccountingTypesDelete)
+                AccountingTypes.Remove(AccountingTypes.Where(level => level.Id == item.Id).First());
+        }
+
+        public async void UpdateAsync()
+        {
+            await DataSender.PutRequest(nameof(AccountingTypes), AccountingTypeSelected.Id.Value, AccountingTypeForEdit);
+            ReadAsync();
+        }
+
+        public bool Validate()
+        {
+            return !string.IsNullOrEmpty(AccountingTypeForEdit.Name);
+        }
     }
 }
