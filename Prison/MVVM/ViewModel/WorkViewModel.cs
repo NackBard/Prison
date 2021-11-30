b1da90bb3ebb59d6259df0c7abd447f543ff5c7d
NackBard <artemguskov5@gmail.com>
2021-11-30 08:59:12 +0300

    Реализовал таблицу Work

--- Prison/MVVM/ViewModel/WorkViewModel.cs
+++ Prison/MVVM/ViewModel/WorkViewModel.cs
@@ -1,12 +1,137 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using Prison.Core;
+using Prison.Data;
+using Prison.Model;
+using System.Collections.ObjectModel;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Prison.MVVM.ViewModel
 {
-    internal class WorkViewModel
+    class WorkViewModel : TableViewModel, ICRUD, ITableModel
     {
+        private ObservableCollection<Work> _works;
+        public ObservableCollection<Work> Works
+        {
+            get => _works;
+            set
+            {
+                _works = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private ObservableCollection<Work> _worksDelete;
+        public ObservableCollection<Work> WorksDelete
+        {
+            get => _worksDelete;
+            set
+            {
+                _worksDelete = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private Work _workDeleteSelected;
+        public Work WorkDeleteSelected
+        {
+            get => _workDeleteSelected;
+            set
+            {
+                _workDeleteSelected = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private Work _workSelected;
+        public Work WorkSelected
+        {
+            get => _workSelected;
+            set
+            {
+                _workSelected = value;
+                WorkForEdit = (Work)_workSelected?.Clone() ?? new Work();
+                OnPropertyChanged();
+            }
+        }
+        private Work _workForEdit;
+        public Work WorkForEdit
+        {
+            get => _workForEdit;
+            set
+            {
+                _workForEdit = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public bool CanDelete => WorkSelected != null;
+        public bool CanCreate => Validate();
+        public bool CanUpdate => WorkSelected != null;
+        public bool CanClear => WorksDelete.Count > 0;
+        public bool CanRecover => WorkDeleteSelected != null;
+
+        public WorkViewModel()
+        {
+            WorkForEdit = new Work();
+            Works = new ObservableCollection<Work>();
+            WorksDelete = new ObservableCollection<Work>();
+            Init();
+        }
+        public void Init()
+        {
+            ReadAsync();
+            DeleteCommand = new RelayCommand(o => Drop(), param => CanDelete);
+            CreateCommand = new RelayCommand(o => CreateAsync());
+            UpdateCommand = new RelayCommand(o => UpdateAsync(), param => CanUpdate);
+            RecoverCommand = new RelayCommand(o => Recover(), param => CanRecover);
+            ClearCommand = new RelayCommand(o => DeleteAsync(), param => CanClear);
+        }
+        public void Recover()
+        {
+            Works.Add(WorkDeleteSelected);
+            WorksDelete.Remove(WorkDeleteSelected);
+        }
+        public void Drop()
+        {
+            WorksDelete.Add(WorkSelected);
+            Works.Remove(WorkSelected);
+        }
+
+        public async void CreateAsync()
+        {
+            if (CanCreate)
+            {
+                WorkForEdit.Id = null;
+                await DataSender.PostRequest(nameof(Works), WorkForEdit);
+                ReadAsync();
+            }
+        }
+
+        public async void DeleteAsync()
+        {
+            foreach (var item in WorksDelete)
+            {
+                var s = await DataSender.DeleteRequest(nameof(Works), item.Id.Value);
+            }
+            WorksDelete.Clear();
+            ReadAsync();
+        }
+
+        public async void ReadAsync()
+        {
+            Works = await ApiConnector.GetAll<Work>(nameof(Works));
+            foreach (var item in WorksDelete)
+                Works.Remove(Works.Where(level => level.Id == item.Id).First());
+        }
+
+        public async void UpdateAsync()
+        {
+            await DataSender.PutRequest(nameof(Works), WorkSelected.Id.Value, WorkForEdit);
+            ReadAsync();
+        }
+
+        public bool Validate()
+        {
+            return !string.IsNullOrEmpty(WorkForEdit.Name);
+        }
     }
 }
