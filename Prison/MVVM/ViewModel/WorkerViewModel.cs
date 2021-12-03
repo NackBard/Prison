860364e07209f264094eeaeba52c900a97e46b32
NackBard <67077453+NackBard@users.noreply.github.com>
2021-12-03 10:26:13 +0300

    Реализовал таблицу Worker

--- Prison/MVVM/ViewModel/WorkerViewModel.cs
+++ Prison/MVVM/ViewModel/WorkerViewModel.cs
@@ -1,12 +1,160 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using Prison.Core;
+using Prison.Data;
+using Prison.Model;
+using System.Collections.ObjectModel;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Prison.MVVM.ViewModel
 {
-    internal class WorkerViewModel
+    class WorkerViewModel : TableViewModel, ICRUD, ITableModel
     {
+        private ObservableCollection<Worker> _workers;
+        public ObservableCollection<Worker> Workers
+        {
+            get => _workers;
+            set
+            {
+                _workers = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private ObservableCollection<Worker> _workersDelete;
+        public ObservableCollection<Worker> WorkersDelete
+        {
+            get => _workersDelete;
+            set
+            {
+                _workersDelete = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private Worker _workerDeleteSelected;
+        public Worker WorkerDeleteSelected
+        {
+            get => _workerDeleteSelected;
+            set
+            {
+                _workerDeleteSelected = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private Worker _workerSelected;
+        public Worker WorkerSelected
+        {
+            get => _workerSelected;
+            set
+            {
+                _workerSelected = value;
+                WorkerForEdit = (Worker)_workerSelected?.Clone() ?? new Worker();
+                OnPropertyChanged();
+            }
+        }
+        private Worker _workerForEdit;
+        public Worker WorkerForEdit
+        {
+            get => _workerForEdit;
+            set
+            {
+                _workerForEdit = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private ObservableCollection<Gender> _genders;
+        public ObservableCollection<Gender> Genders
+        {
+            get => _genders;
+            set
+            {
+                _genders = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private ObservableCollection<Post> _posts;
+        public ObservableCollection<Post> Posts
+        {
+            get => _posts;
+            set
+            {
+                _posts = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public bool CanDelete => WorkerSelected != null;
+        public bool CanCreate => Validate();
+        public bool CanUpdate => WorkerSelected != null;
+        public bool CanClear => WorkersDelete.Count > 0;
+        public bool CanRecover => WorkerDeleteSelected != null;
+
+        public WorkerViewModel()
+        {
+            WorkerForEdit = new Worker();
+            Workers = new ObservableCollection<Worker>();
+            WorkersDelete = new ObservableCollection<Worker>();
+            Init();
+        }
+        public void Init()
+        {
+            ReadAsync();
+            DeleteCommand = new RelayCommand(o => Drop(), param => CanDelete);
+            CreateCommand = new RelayCommand(o => CreateAsync());
+            UpdateCommand = new RelayCommand(o => UpdateAsync(), param => CanUpdate);
+            RecoverCommand = new RelayCommand(o => Recover(), param => CanRecover);
+            ClearCommand = new RelayCommand(o => DeleteAsync(), param => CanClear);
+        }
+        public void Recover()
+        {
+            Workers.Add(WorkerDeleteSelected);
+            WorkersDelete.Remove(WorkerDeleteSelected);
+        }
+        public void Drop()
+        {
+            WorkersDelete.Add(WorkerSelected);
+            Workers.Remove(WorkerSelected);
+        }
+
+        public async void CreateAsync()
+        {
+            if (CanCreate)
+            {
+                WorkerForEdit.Id = null;
+                await DataSender.PostRequest(nameof(Workers), WorkerForEdit);
+                ReadAsync();
+            }
+        }
+
+        public async void DeleteAsync()
+        {
+            foreach (var item in WorkersDelete)
+                await DataSender.DeleteRequest(nameof(Workers), item.Id.Value);
+            WorkersDelete.Clear();
+            ReadAsync();
+        }
+
+        public async void ReadAsync()
+        {
+            Posts = await ApiConnector.GetAll<Post>(nameof(Posts));
+            Genders = await ApiConnector.GetAll<Gender>(nameof(Genders));
+            Workers = await ApiConnector.GetAll<Worker>(nameof(Workers));
+            foreach (var item in WorkersDelete)
+                Workers.Remove(Workers.First(level => level.Id == item.Id));
+        }
+
+        public async void UpdateAsync()
+        {
+            await DataSender.PutRequest(nameof(Workers), WorkerSelected.Id.Value, WorkerForEdit);
+            ReadAsync();
+        }
+
+        public bool Validate()
+        {
+            return false;
+        }
     }
 }
+
