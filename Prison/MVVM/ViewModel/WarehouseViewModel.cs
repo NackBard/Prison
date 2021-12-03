823b32b2cd54a9d89dc421d839bb5cbdd0544036
NackBard <67077453+NackBard@users.noreply.github.com>
2021-12-03 09:47:38 +0300

    Реализовал таблицу Warehouse, исправил    отображение столбцов таблиц Prisoner и Product

--- Prison/MVVM/ViewModel/WarehouseViewModel.cs
+++ Prison/MVVM/ViewModel/WarehouseViewModel.cs
@@ -1,12 +1,149 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using Prison.Core;
+using Prison.Data;
+using Prison.Model;
+using System.Collections.ObjectModel;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Prison.MVVM.ViewModel
 {
-    internal class WarehouseViewModel
+    class WarehouseViewModel : TableViewModel, ICRUD, ITableModel
     {
+        private ObservableCollection<Warehouse> _warehouses;
+        public ObservableCollection<Warehouse> Warehouses
+        {
+            get => _warehouses;
+            set
+            {
+                _warehouses = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private ObservableCollection<Warehouse> _warehousesDelete;
+        public ObservableCollection<Warehouse> WarehousesDelete
+        {
+            get => _warehousesDelete;
+            set
+            {
+                _warehousesDelete = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private Warehouse _warehouseDeleteSelected;
+        public Warehouse WarehouseDeleteSelected
+        {
+            get => _warehouseDeleteSelected;
+            set
+            {
+                _warehouseDeleteSelected = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private Warehouse _warehouseSelected;
+        public Warehouse WarehouseSelected
+        {
+            get => _warehouseSelected;
+            set
+            {
+                _warehouseSelected = value;
+                WarehouseForEdit = (Warehouse)_warehouseSelected?.Clone() ?? new Warehouse();
+                OnPropertyChanged();
+            }
+        }
+        private Warehouse _warehouseForEdit;
+        public Warehouse WarehouseForEdit
+        {
+            get => _warehouseForEdit;
+            set
+            {
+                _warehouseForEdit = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private ObservableCollection<Product> _products;
+        public ObservableCollection<Product> Products
+        {
+            get => _products;
+            set
+            {
+                _products = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public bool CanDelete => WarehouseSelected != null;
+        public bool CanCreate => Validate();
+        public bool CanUpdate => WarehouseSelected != null;
+        public bool CanClear => WarehousesDelete.Count > 0;
+        public bool CanRecover => WarehouseDeleteSelected != null;
+
+        public WarehouseViewModel()
+        {
+            WarehouseForEdit = new Warehouse();
+            Warehouses = new ObservableCollection<Warehouse>();
+            WarehousesDelete = new ObservableCollection<Warehouse>();
+            Init();
+        }
+        public void Init()
+        {
+            ReadAsync();
+            DeleteCommand = new RelayCommand(o => Drop(), param => CanDelete);
+            CreateCommand = new RelayCommand(o => CreateAsync());
+            UpdateCommand = new RelayCommand(o => UpdateAsync(), param => CanUpdate);
+            RecoverCommand = new RelayCommand(o => Recover(), param => CanRecover);
+            ClearCommand = new RelayCommand(o => DeleteAsync(), param => CanClear);
+        }
+        public void Recover()
+        {
+            Warehouses.Add(WarehouseDeleteSelected);
+            WarehousesDelete.Remove(WarehouseDeleteSelected);
+        }
+        public void Drop()
+        {
+            WarehousesDelete.Add(WarehouseSelected);
+            Warehouses.Remove(WarehouseSelected);
+        }
+
+        public async void CreateAsync()
+        {
+            if (CanCreate)
+            {
+                WarehouseForEdit.Id = null;
+                await DataSender.PostRequest(nameof(Warehouses), WarehouseForEdit);
+                ReadAsync();
+            }
+        }
+
+        public async void DeleteAsync()
+        {
+            foreach (var item in WarehousesDelete)
+            {
+                await DataSender.DeleteRequest(nameof(Warehouses), item.Id.Value);
+            }
+            WarehousesDelete.Clear();
+            ReadAsync();
+        }
+
+        public async void ReadAsync()
+        {
+            Products = await ApiConnector.GetAll<Product>(nameof(Products));
+            Warehouses = await ApiConnector.GetAll<Warehouse>(nameof(Warehouses));
+            foreach (var item in WarehousesDelete)
+                Warehouses.Remove(Warehouses.Where(level => level.Id == item.Id).First());
+        }
+
+        public async void UpdateAsync()
+        {
+            await DataSender.PutRequest(nameof(Warehouses), WarehouseSelected.Id.Value, WarehouseForEdit);
+            ReadAsync();
+        }
+
+        public bool Validate()
+        {
+            return false;
+        }
     }
 }
