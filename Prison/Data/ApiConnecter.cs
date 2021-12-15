80d25f30374f841ff6d174e22c26673eff719544
NackBard <artemguskov5@gmail.com>
2021-12-15 07:06:09 +0300

    Реализовал метод экспорта таблицы

--- Prison/Data/ApiConnecter.cs
+++ Prison/Data/ApiConnecter.cs
@@ -9,6 +9,9 @@ namespace Prison.Data
         public static async Task<ObservableCollection<T>> GetAll<T>(string tableName)
         {
             string responseBody = await GetRequest(tableName);
+            var df = TableHelper.jsonToCSV(responseBody);
+            var ds = TableHelper.CsvToJson(df.Replace("\n","").Split("\r"));
+            var zcx = JsonConvert.SerializeObject(ds);
             return (ObservableCollection<T>)JsonConvert.DeserializeObject(responseBody, typeof(ObservableCollection<T>));
         }
 
@@ -17,5 +20,6 @@ namespace Prison.Data
             string responseBody = await PostRequest(value);
             return (T)JsonConvert.DeserializeObject(responseBody, typeof(T));
         }
+
     }
 }
--- Prison/Data/TableHelper.cs
+++ Prison/Data/TableHelper.cs
@@ -1,4 +1,14 @@
-﻿using System.Collections.ObjectModel;
+﻿
+
+using CsvHelper;
+using CsvHelper.Configuration;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Data;
+using System.Dynamic;
+using System.IO;
 using System.Linq;
 
 namespace Prison.Data
@@ -12,6 +22,35 @@ namespace Prison.Data
             foreach (var item in s)
                 sorter.Add(item);
             return sorter;
+        }
+
+        public static string jsonToCSV(string jsonContent)
+        {
+            var expandos = JsonConvert.DeserializeObject<ExpandoObject[]>(jsonContent);
+            CsvConfiguration configuration = new CsvConfiguration(System.Globalization.CultureInfo.CurrentCulture);
+            using (var writer = new StringWriter())
+            {
+                using (var csv = new CsvWriter(writer,configuration))
+                {
+                    csv.WriteRecordsAsync(expandos as IEnumerable<dynamic>);
+                }
+
+                return writer.ToString();
+            }
+        }
+
+        public static IEnumerable<JObject> CsvToJson(IEnumerable<string> csvLines)
+        {
+            var csvLinesList = csvLines.ToList();
+
+            var header = csvLinesList[0].Split(',');
+            for (int i = 1; i < csvLinesList.Count; i++)
+            {
+                var thisLineSplit = csvLinesList[i].Split(',');
+                var pairedWithHeader = header.Zip(thisLineSplit, (h, v) => new KeyValuePair<string, string>(h, v));
+
+                yield return new JObject(pairedWithHeader.Select(j => new JProperty(j.Key, j.Value)));
+            }
         }
 
         //public ObservableCollection<T> Filter<T>(ObservableCollection<T> sorter, string propertyName, object value)
